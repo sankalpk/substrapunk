840b40f9a6b3ed5abafc8913ee481054744537a5
Sankalp Kulshreshtha <sankalp221@gmail.com>
2021-09-19 22:46:11 -1000

    Add raffle endpoints

---
+++ app/controllers/raffles_controller.rb
@@ -0,0 +1,33 @@
+class RafflesController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    before_action :set_raffle, only: %i[ show submit ]
+
+    def show
+        render json: json
+    end
+
+    def submit
+        @raffle.update!(submitted_raffle: true)
+        render json: json
+    end
+
+    private
+
+    def set_raffle
+        @raffle = Raffle.find_by_address(params[:address])
+        if(@raffle)
+            @raffle.update_has_bird!
+        else
+            @raffle = Raffle.new(address: params[:address])
+            @raffle.update_has_bird!
+        end
+    end
+
+    def json
+        {
+            address: @raffle.address,
+            has_bird: @raffle.has_bird,
+            submitted_raffle: @raffle.submitted_raffle
+        }
+    end
+end
--- app/models/raffle.rb
+++ app/models/raffle.rb
@@ -1,3 +1,15 @@
 class Raffle < ApplicationRecord
     validates_presence_of :address
+    def update_has_bird!
+        self.has_bird = kanaria_response.size > 0
+        save!
+    end
+
+    def kanaria_response
+        JSON.parse(
+            HTTP.get("https://kanaria.rmrk.app/api/account/#{address}")
+                .body
+                .to_s
+        )
+    end
 end
