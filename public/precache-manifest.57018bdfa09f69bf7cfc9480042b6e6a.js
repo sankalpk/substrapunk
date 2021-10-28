b59e9ae6ebdf64889b00ac5a2662fa2dcf6e8c64
Sankalp Kulshreshtha <sankalp221@gmail.com>
2021-10-28 13:35:18 -0400

    Add frontend

---
+++ public/precache-manifest.57018bdfa09f69bf7cfc9480042b6e6a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b85b08f81c9dd78c07bcd1d98170c5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f53e28b17f3f60ce609",
+    "url": "/static/css/main.f49714b8.chunk.css"
+  },
+  {
+    "revision": "cf8c6005b6002036f7b1",
+    "url": "/static/js/2.af9eea82.chunk.js"
+  },
+  {
+    "revision": "54596480d763921c1d44581153e7a2bb",
+    "url": "/static/js/2.af9eea82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f53e28b17f3f60ce609",
+    "url": "/static/js/main.bc30318a.chunk.js"
+  },
+  {
+    "revision": "7a6956e196ccd2e4ccc8856708b02847",
+    "url": "/static/js/main.bc30318a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34848c4485a0a280a338",
+    "url": "/static/js/runtime-main.bd198a27.js"
+  },
+  {
+    "revision": "cb3cd475102678857655b03ff3b53471",
+    "url": "/static/media/PunkRaffle.cb3cd475.glb"
+  },
+  {
+    "revision": "d0f4e74163d8cf0f9875a74e5de4c69e",
+    "url": "/static/media/PunkRaffle_l.d0f4e741.glb"
+  },
+  {
+    "revision": "33600f905be5d4827a8b243b349c70e1",
+    "url": "/static/media/RaffleTicket.33600f90.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4ffd4fe7945af123788bf5888296c696",
+    "url": "/static/media/github.4ffd4fe7.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "6025f4a64eb32ca87e51896c79805ec3",
+    "url": "/static/media/header3.6025f4a6.png"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/static/media/nucleo-icons.f82ec6ba.ttf"
+  }
+]);
